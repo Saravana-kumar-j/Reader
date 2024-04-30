7604ea50c7f45f9e7a4cd7fb4b7a47708825f124
Saravana-kumar-j <saravanakumarj.net@gmail.com>
2024-04-30 18:01:20 +0530

    local commit

---
+++ app/src/main/java/com/example/reader/Splash_screen.java
@@ -0,0 +1,30 @@
+package com.example.reader;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash_screen.this, Welcome_activity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2700); // Change this to the desired duration of the splash screen in milliseconds
+    }
+}
---
+++ app/src/main/java/com/example/reader/Welcome_activity.java
@@ -0,0 +1,47 @@
+package com.example.reader;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Welcome_activity extends AppCompatActivity {
+    private Button register;
+    private Button login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_welcome);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        register = findViewById(R.id.welcome_register);
+        login =  findViewById(R.id.welcome_login);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =  new Intent(Welcome_activity.this, register_activity.class);
+                startActivity(intent);
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Welcome_activity.this, Login_activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
