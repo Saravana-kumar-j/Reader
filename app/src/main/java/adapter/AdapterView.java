9dfab13023c263d06c0da337bb4c4dc97259b13a
Saravana-kumar-j <saravanakumarj.net@gmail.com>
2024-05-14 19:46:54 +0530

    Fragment

--- app/src/main/java/adapter/AdapterView.java
+++ app/src/main/java/adapter/AdapterView.java
@@ -1,4 +1,27 @@
 package adapter;
 
-public class AdapterView {
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.ArrayList;
+
+public class AdapterView extends FragmentStateAdapter {
+    ArrayList<Fragment> array;
+    public AdapterView(@NonNull FragmentActivity fragmentActivity, ArrayList<Fragment> array) {
+        super(fragmentActivity);
+        this.array = array;
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        return array.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return array.size();
+    }
 }
--- app/src/main/java/com/example/reader/MainActivity.java
+++ app/src/main/java/com/example/reader/MainActivity.java
@@ -3,21 +3,37 @@ package com.example.reader;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+import adapter.AdapterView;
+import fragment.FragmentHome;
+import fragment.FragmentNotification;
+import fragment.FragmentProfile;
+
+public class MainActivity extends AppCompatActivity {
+    ViewPager2 pageMain;
     FirebaseAuth auth;
     Button button;
     TextView textView;
     FirebaseUser user;
+    ArrayList<Fragment> fragmentArrayList = new ArrayList<>();
+    BottomNavigationView bottomNav;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +45,51 @@ public class MainActivity extends AppCompatActivity {
         button = findViewById(R.id.btn_logout);
         textView = findViewById(R.id.user_details);
         user = auth.getCurrentUser();
+
+//        Pager Fragment
+        pageMain = findViewById(R.id.pagerMain);
+        fragmentArrayList.add(new FragmentHome());
+        fragmentArrayList.add(new FragmentNotification());
+        fragmentArrayList.add(new FragmentProfile());
+        bottomNav = findViewById(R.id.bottomNav);
+
+        AdapterView adapterView = new AdapterView(this, fragmentArrayList);
+
+        pageMain.setAdapter(adapterView);
+        pageMain.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position){
+                    case 0:
+                        bottomNav.setSelectedItemId(R.id.itHome);
+                        break;
+                    case 1:
+                        bottomNav.setSelectedItemId(R.id.itNotification);
+                        break;
+                    case 2:
+                        bottomNav.setSelectedItemId(R.id.itProfile);
+                        break;
+                }
+
+                super.onPageSelected(position);
+            }
+        });
+
+        bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if(id == R.id.itHome){
+                    pageMain.setCurrentItem(0);
+                } else if (id == R.id.itNotification) {
+                    pageMain.setCurrentItem(1);
+                } else if (id == R.id.itProfile) {
+                    pageMain.setCurrentItem(2);
+                }
+                return true;
+            }
+        });
 
         if(user == null){
             Intent intent = new Intent(getApplicationContext(), Login_activity.class);
@@ -47,5 +108,6 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+
     }
 }
